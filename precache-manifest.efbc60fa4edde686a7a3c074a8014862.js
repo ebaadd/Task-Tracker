9f0bbe479cef76919575f9b0699237308e24eb41
ebd rhn <ebadrhmn0@gmail.com>
2022-12-04 23:12:25 +0530

    Updates

---
+++ precache-manifest.efbc60fa4edde686a7a3c074a8014862.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c33ca1e5823231d07fff",
+    "url": "/Task-Tracker/static/css/main.b42e5cf6.chunk.css"
+  },
+  {
+    "revision": "c33ca1e5823231d07fff",
+    "url": "/Task-Tracker/static/js/main.c33ca1e5.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/Task-Tracker/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "73f474f2958dae0e026d",
+    "url": "/Task-Tracker/static/js/2.73f474f2.chunk.js"
+  },
+  {
+    "revision": "766af6c0096469bf8a2f",
+    "url": "/Task-Tracker/static/js/runtime~main.766af6c0.js"
+  },
+  {
+    "revision": "e8d9bd0ac8532d48997c7b9edb835e85",
+    "url": "/Task-Tracker/index.html"
+  }
+];
